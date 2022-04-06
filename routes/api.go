a1967386e305e580f6feb213214b2c9cc9355d8b
Eric <273086429@qq.com>
2022-04-06 15:39:55 +0800

    发送邮箱验证码接口

--- routes/api.go
+++ routes/api.go
@@ -27,6 +27,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 			// 发送手机验证码
 			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+			// 发送邮箱验证码
+			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
 		}
 	}
 }
