eb18f7ce89328521a2839434a215f2eb2a38f498
Eric <273086429@qq.com>
2022-04-07 10:38:37 +0800

    通过手机号找回密码

--- routes/api.go
+++ routes/api.go
@@ -41,6 +41,10 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/login/using-password", lgc.LoginByPassword)
 			// 刷新 JWT
 			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
+
+			// 重置密码
+			pwc := new(auth.PasswordController)
+			authGroup.POST("/password-reset/using-phone", pwc.ResetByPhone)
 		}
 	}
 }
